c5bfb9ae0eb31eb17a19c0e3edcccfadf97e0b76
emibcn <emi@algorismia.com>
2021-05-22 23:07:11 +0200

    Add the code and all the envs: eslint, prettier, builder, tester

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import * as core from '@actions/core';
+import gradientBadge from 'gradient-badge';
+import fs from 'fs';
+
+const createBadgeFromInputs = () => {
+  try {
+    // Get action inputs
+    const label = core.getInput('label');
+    const labelColor = core.getInput('label-color');
+    const status = `${core.getInput('status')}`; // Ensure string
+    const gradient = core
+      .getInput('color')
+      .split(',') // Color gradient as Array
+      .map((color) => color.trim(' ')); // Clean spaces
+    const style = core.getInput('style');
+    const icon = core.getInput('icon');
+    const iconWidth = core.getInput('icon-width');
+    const scale = core.getInput('scale');
+    const path = core.getInput('path');
+
+    const inputs = {
+      label,
+      labelColor,
+      status,
+      gradient,
+      style,
+      icon: icon?.length ? icon : null,
+      iconWidth,
+      scale,
+    };
+
+    console.log(' badge using the given inputs and defaults: ', inputs);
+
+    // Generate the badge
+    const svgString = gradientBadge(inputs);
+
+    // Output badge contents to Action output
+    core.setOutput('badge', svgString);
+
+    // If path is defined, save SVG data to that file
+    if (path && path.length) {
+      console.log(`Write data to file ${path}...`);
+
+      // In case an error occurred writing file,
+      // exception is thrown and success messsage is not printed
+      fs.writeFileSync(path, svgString);
+      console.log('Data saved succesfully.');
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+export default createBadgeFromInputs;
---
+++ src/index.test.js
@@ -0,0 +1,5 @@
+import createBadgeFromInputs from './index';
+
+it('Does not crash', () => {
+  createBadgeFromInputs();
+});
