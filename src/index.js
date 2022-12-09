e493b2ff5ba3ddd7f5805816bb332f5ba1f179c1
emibcn <emi@algorismia.com>
2022-12-09 12:06:35 +0100

    Fix https://deepsource.io/gh/emibcn/github-badge-action/run/758fc6ba-5019-4a2b-ab32-23242cc066c3/javascript/JS-C1003 - Avoid using wildcard imports - JS-C1003

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-import * as core from '@actions/core'
+import {
+  getInput,
+  debug,
+  info,
+  setOutput,
+  error,
+  setFailed
+} from '@actions/core'
 import gradientBadge from 'gradient-badge'
 import fs from 'fs'
 import defaultInputMap from './defaultInputMap'
@@ -14,19 +21,19 @@ const createBadgeFromInputs = ({
     const inputs = Object.entries(inputMap).reduce(
       (acc, [key, inputName]) => ({
         ...acc,
-        [key]: core.getInput(inputName)
+        [key]: getInput(inputName)
       }),
       {}
     )
 
-    core.debug('Received inputs:', inputs)
+    debug('Received inputs:', inputs)
 
     // Fix some inputs
     for (const [key, fn] of Object.entries(inputFixes)) {
       inputs[key] = fn(inputs)
     }
 
-    core.info('Generate badge using the given inputs and defaults:', inputs)
+    info('Generate badge using the given inputs and defaults:', inputs)
 
     // Generate the badge
     const { path, ...gradientBadgeOptions } = inputs
@@ -34,22 +41,22 @@ const createBadgeFromInputs = ({
 
     // Output badge contents to Action output
     if (outputName?.length) {
-      core.info("Write data to action's output 'badge'...")
-      core.setOutput(outputName, svgString)
+      info("Write data to action's output 'badge'...")
+      setOutput(outputName, svgString)
     }
 
     // If path is defined, save SVG data to that file
     if (path?.length) {
-      core.info(`Write data to file ${path}...`)
+      info(`Write data to file ${path}...`)
 
       // In case an error occurred writing file,
       // exception is thrown and success messsage is not printed
       fs.writeFileSync(path, svgString)
-      core.info('Data saved succesfully.')
+      info('Data saved succesfully.')
     }
-  } catch (error) {
-    core.error(error)
-    core.setFailed(error.message)
+  } catch (err) {
+    error(err)
+    setFailed(err.message)
   }
 }
 
